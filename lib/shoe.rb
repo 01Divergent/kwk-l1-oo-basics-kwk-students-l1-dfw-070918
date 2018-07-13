36cee8d301b71c500a520871c2f6c072d40d37db
01Divergent <iphone.claudia@icloud.com>
2018-07-13 19:35:50 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,44 @@
 # Make your shoe class here!
+class Shoe
+  def initialize(brand,  color, size, material, condition = "new", cobble)
+    @brand = brand
+    @color = color 
+    @size = size
+    @material = material
+    @condition = condition
+    @cobble = cobble 
+  end
+  def brand=(brand)
+    @brand = brand
+  end
+  def brand
+    @brand
+  end 
+  def color=(color)
+    @color = color
+  end
+  def color
+    @color
+  end
+  def size=(size)
+    @size = size
+  end
+  def size
+    @size
+  end
+  def material=(material)
+    @material = material
+  end
+  def material
+    @material
+  end
+  def condition=(condition)
+    @condition = condition
+  end
+  def condition
+    @condition
+  end
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
+end
